7ba78b1b27cf8b0a1291bda1440127b0d7d1a80b
Ravkeerat02 <42888466+Ravkeerat02@users.noreply.github.com>
2023-03-09 02:41:34 -0500

    complete lab

---
+++ Lab05-101344680/exercise-1/helloworld.js
@@ -0,0 +1,8 @@
+// var greeter = function (person) {
+//     return "Hello, " + person;
+// }
+// greeter("World");
+var greeter = function (firstName, lastName) {
+    console.log(firstName + " " + lastName);
+};
+greeter("Jane", "Doe");
